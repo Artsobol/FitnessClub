edcfb7649e2404750860f026be12d9fd9cbcd411
Artsobol <artsobol05@yandex.ru>
2026-02-14 20:12:18 +0300

    feat(security): configure spring security and JWT filter

--- src/main/java/io/github/artsobol/fitnessclub/infrastructure/security/user/UserDetailsImpl.java
+++ src/main/java/io/github/artsobol/fitnessclub/infrastructure/security/user/UserDetailsImpl.java
@@ -1,6 +1,6 @@
 package io.github.artsobol.fitnessclub.infrastructure.security.user;
 
-import io.github.artsobol.fitnessclub.feature.user.User;
+import io.github.artsobol.fitnessclub.feature.user.dto.User;
 import lombok.RequiredArgsConstructor;
 import org.jspecify.annotations.NonNull;
 import org.jspecify.annotations.Nullable;
