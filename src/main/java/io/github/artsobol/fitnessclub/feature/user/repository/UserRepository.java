3e53f67c80d38fe05c5ec94f1d0b572a3208021a
Artsobol <artsobol05@yandex.ru>
2026-02-14 20:11:50 +0300

    refactor(domain): add user repository

--- src/main/java/io/github/artsobol/fitnessclub/feature/user/repository/UserRepository.java
+++ src/main/java/io/github/artsobol/fitnessclub/feature/user/repository/UserRepository.java
@@ -1,6 +1,6 @@
 package io.github.artsobol.fitnessclub.feature.user.repository;
 
-import io.github.artsobol.fitnessclub.feature.user.User;
+import io.github.artsobol.fitnessclub.feature.user.dto.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -9,4 +9,6 @@ import java.util.UUID;
 public interface UserRepository extends JpaRepository<User, UUID> {
 
     Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
 }
